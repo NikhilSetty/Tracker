aad58319d842885acc126cb3cdfc089cb1315b08
Nikhil Ravishankar <nravishankar@vmware.com>
2016-05-20 16:07:08 +0530

    Service to continuously get GPS location.

---
+++ app/src/main/java/com/ve/tracker/tracker/Helper/StaticHelper.java
@@ -0,0 +1,9 @@
+package com.ve.tracker.tracker.Helper;
+
+/**
+ * Created by nravishankar on 5/20/2016.
+ */
+public class StaticHelper {
+    static final public String UPDATE_UI = "com.ve.tracker.TrackerService.DataReceived";
+    static final public String UPDATE_UI_MESSAGE = "com.ve.tracker.TrackerService.DataMessage";
+}
--- app/src/main/java/com/ve/tracker/tracker/MainActivity.java
+++ app/src/main/java/com/ve/tracker/tracker/MainActivity.java
@@ -1,16 +1,78 @@
 package com.ve.tracker.tracker;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.LocationManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ve.tracker.tracker.Helper.StaticHelper;
+import com.ve.tracker.tracker.Service.TrackerService;
 
 public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String s = intent.getStringExtra(StaticHelper.UPDATE_UI_MESSAGE);
+                Toast.makeText(context, s, Toast.LENGTH_LONG).show();
+            }
+        };
+
+
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+
+        if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
+    }
+
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(this).registerReceiver((receiver),
+                new IntentFilter(StaticHelper.UPDATE_UI)
+        );
+    }
+
+    @Override
+    protected void onStop() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
+        super.onStop();
     }
 
     @Override
@@ -33,5 +95,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void StartService(View view) {
+        Intent intent = new Intent(this, TrackerService.class);
+        startService(intent);
     }
 }
